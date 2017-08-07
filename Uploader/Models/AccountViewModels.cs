412898adddf6d382466cd43172940ba027b77721
Jay <jayadi.kurniawan@gmail.com>
2017-08-08 01:06:53 +0700

    Edit Register page, add checkbox to register as Admin

--- Uploader/Models/AccountViewModels.cs
+++ Uploader/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Uploader.Models
@@ -79,6 +80,8 @@ namespace Uploader.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public bool Admin { get; set; }
     }
 
     public class ResetPasswordViewModel
